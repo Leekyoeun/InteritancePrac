a7618a26ddb01119dd9843454168febf15ef779f
Leekyoeun <s2018s35@e-mirim.hs.kr>
2020-06-10 13:51:02 +0900

    초기 커밋.

---
+++ Dog.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Dog
+{
+    public int age { get; set; }
+    public string Color { get; set; }
+
+    public Dog() { this.age = 0; }
+
+    public void Eat() { Console.WriteLine("³È³È"); }
+    public void Slee() { Consaole.WriteLine("ÄðÄð"); }
+
---
+++ InteritancePrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteritancePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> dogs = new List<Dog> { new Dog(), new Dog(), new Dog() };
+            List<Cat> cats = new List<Cat> { new Cat(), new Cat(), new Cat() };
+
+        }
+    }
+}
